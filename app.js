2f6735b0cef1825e9b57511c443334987287dfd4
rancongjie <95633676@qq.com>
2016-03-11 16:36:20 +0800

    后端 firstcommit

---
+++ app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by rancongjie on 16/3/11.
+ */
+var express = require('./config/express');
+
+var app = express();
+
+module.exports = app;
+
---
+++ config/express.js
@@ -0,0 +1,34 @@
+/**
+ * Created by rancongjie on 16/3/11.
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+
+module.exports = function () {
+  console.log('init express');
+  var app = express();
+
+  app.use(bodyParser.json());
+
+  app.use(function (req, res, next) {
+    res.status(404);
+    try {
+      return res.json('not found');
+    } catch (e) {
+      console.error('send err');
+    }
+  });
+  app.use(function (err, req, res, next) {
+
+    if (!err) {
+      return next();
+    }
+    res.status(500);
+    try {
+      return res.json(err.message || 'server error');
+    } catch (e) {
+      console.log('send err');
+    }
+  });
+  return app;
+};
